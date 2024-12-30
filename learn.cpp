abc63e7112233d36c6ae7062a0ee10ac955e03ab
jzx531 <115254510+jzx531@users.noreply.github.com>
2024-12-30 21:16:49 +0800

    new

--- learn.cpp
+++ learn.cpp
@@ -1,82 +1,45 @@
-// #include <iostream>
-
-// // 递归版本：存储多个元素的tuple
-// template<typename T, typename... Args>
-// class tuple {
-// private:
-//     T data;               // 当前元素
-//     tuple<Args...>next; // 下一个元素
-
-// public:
-//     tuple(T t, Args... args) : data(t), next(tuple<Args...>(args...)) {}
-//     tuple(const tuple<T, Args...>& other) : data(other.data), next(other.next) {}
-
-
-//     void print() const {
-//         std::cout << data << " ";
-//         next.print(); // 递归打印后续元素
-//     }
-
-//     T getdata() const {
-//         return data;  // 返回当前元素
-//     }
-
-//     tuple<Args...> getnext() {
-//         return next;  // 返回下一个元素
-//     }
-// };
-
-// // 特化版本：只有一个元素的tuple
-// template<typename T>
-// class tuple<T> {
-// private:
-//     T data;
-
-// public:
-//     tuple(T t) : data(t) {}
-
-//     void print() const {
-//         std::cout << data << std::endl;  // 只有一个元素时，打印
-//     }
-
-//     T getdata() const {
-//         return data;
-//     }
-
-// };
-
-
-// int main() {
-//     tuple<int, char, double, float> t(1, 'a', 3.14, 2.71);
-    
-//     // 打印所有元素
-//     t.print();  // 输出: 1 a 3.14 2.71
-//     // 访问第2个元素
-    
-//     return 0;
-// }
-
-// #include <iostream>
-// #include "CirDoublyList.hpp"
-
-// int main() {
-//     int a[] = {1, 2, 3, 4, 5};
-//     CirDoublyList<int> list(a, 5);
-//     std::cout<<list;
-//     list.reverse();
-//     std::cout<<list;
-//     return 0;
-// }
-
 #include <iostream>
-#include "sort.hpp"
-int main() {
-    int a[] = {5, 3, 8, 6, 2, 7, 1, 4};
-    int n = sizeof(a) / sizeof(a[0]);
-    QuickSort(a, 0,n-1);
-    PrintArray(a, n);
-    return 0;
+#include "mingw.shared_mutex.h"
+#include "mingw.thread.h"
+#include <vector>
+#include <atomic>
+
+#define DATA_SIZE 10
+std::shared_mutex smtx;
+std::mutex mtx;
+std::atomic<bool> ready(false);
+std::vector<int> data;
+void init_data() {
+    for (int i = 0; i < DATA_SIZE; i++) {
+        data.push_back(i);
+    }
+    ready .store(true, std::memory_order_release);
 }
 
 
 
+void thread_func(int id) {
+    // std::lock_guard<std::mutex> lock(mtx);
+    while(! ready.load(std::memory_order_acquire))
+    while(true) {
+        std::shared_lock<std::shared_mutex> lock(smtx);
+        // for (int i = 0; i < DATA_SIZE; i++) {
+            int i = rand() % DATA_SIZE;
+            std::cout << id << "THREAD "<< data[i] << std::endl;
+            std::this_thread::yield();
+        // }
+    }
+}
+
+int main() {
+    std::thread t(init_data);
+    std::thread t1(thread_func,1);
+    std::thread t2(thread_func,2);
+    
+    t.join();
+    t1.join();
+    t2.join();
+    
+   
+    return 0;
+}
